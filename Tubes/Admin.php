06fa0e26de39311f5e663196cd0bf657c085a3d7
Lychtael <jitabilhaq12@gmail.com>
2023-05-30 17:02:15 +0700

    push pertemuan13

--- Tubes/Admin.php
+++ Tubes/Admin.php
@@ -7,10 +7,57 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Tiro+Devanagari+Sanskrit&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet" />
 </head>
 
 <body>
-
+    <!-- header -->
+    <div class="header">
+        <div class="container-fluid bg-black bg-gradient text-white shadow p-2">
+            <div class="top">
+                <div class="d-flex">
+                    <div class="p-2 flex-grow-1">
+                        <div class="logo">
+                            <a href="index.php">The Etherys</a>
+                        </div>
+                    </div>
+                    <div class="top-tombol">
+                        <div class="tombol-login bg-black bg-gradient p-1">
+                            <div class="p-3 fs-5">
+                                <span>
+                                    <a href="LogIn.php">LOG IN</a>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="tombol-login bg-black bg-gradient p-1">
+                            <div class="p-3 fs-5">
+                                <span>
+                                    <a href="LogIn.php">SIGN UP</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="bg-dark bg-gradient navbar-sm">
+            <div class="nav_list">
+                <ul>
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="#game">GAME</a></li>
+                    <li><a href="#news">NEWS</a></li>
+                    <li>
+                        <a href="https://forum.square-enix.com/ffxiv/forums/667" target="_blank">FORUM</a>
+                    </li>
+                    <li><a href="#media">MEDIA</a></li>
+                    <li><a href="#about">ABOUT US</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </body>
 
 </html>
---
+++ kuliah/pertemuan13/views/index.view.php
@@ -0,0 +1,42 @@
+<?php require('partials/header.php'); ?>
+<?php require('partials/nav.php'); ?>
+
+<div class="container mt-3">
+  <h1>Halaman Home</h1>
+
+  <h3>Daftar Mahasiswa</h3>
+
+  <a href="tambah.php" class="btn btn-primary">Tambah Data Mahasiswa</a>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Gambar</th>
+        <th scope="col">NIM</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Email</th>
+        <th scope="col">Jurusan</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($students as $student) : ?>
+        <tr>
+          <th scope="row"><?= $student["id"]; ?></th>
+          <td><img src="img/default.png" width="100" class="rounded-circle"></td>
+          <td><?= $student["nim"]; ?></td>
+          <td><?= $student["nama"]; ?></td>
+          <td><?= $student["email"]; ?></td>
+          <td><?= $student["jurusan"]; ?></td>
+          <td>
+            <a href="ubah.php?id=<?= $student['id']; ?>">ubah</a> |
+            <a href="hapus.php?id=<?= $student['id']; ?>" onclick="return confirm('afkh antum yaqueen?');">hapus</a>
+          </td>
+        </tr>
+      <?php endforeach ?>
+    </tbody>
+  </table>
+</div>
+
+<?php require('partials/footer.php'); ?>
